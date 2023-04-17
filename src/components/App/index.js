5d5c67968c52c74374fe538640984875848a959e
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-17 17:41:46 +0100

    task 1 & 2 - pokemon viewer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
-import PokemonViewer from "../PokemonViewer";
-
+import PokemonViewer from '../PokemonViewer';
+// PokemonViewer();
 function App() {
-  const [id, setId] = useState();
+	const [id, setId] = useState(0);
 
-  function handleClick() {
-    // TODO: Task 2 - Set id to be random number between 1 and 151
-  }
+	function handleClick() {
+		// TODO: Task 2 - Set id to be random number between 1 and 151
+		setId(Math.floor(Math.random() * 151) + 1);
+		console.log(id);
+	}
 
-  return (
-    <div className="App">
-      {/* TODO: Task 2 -  call handleClick when button clicked */}
-      <button>Get Random Pokemon</button>
-      {/* TODO: Task 2 -  hand down id as a prop */}
-      <PokemonViewer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			{/* TODO: Task 2 -  call handleClick when button clicked */}
+			<button onClick={handleClick}>Get Random Pokemon</button>
+			{/* TODO: Task 2 -  hand down id as a prop */}
+			<PokemonViewer id={id} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,8 +1,40 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 function PokemonViewer({ id }) {
-  //TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
-  // TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
-  // HINT: you will need useState and useEffect!
-  return <div className="pokemon-viewer"></div>;
+	const [name, setName] = useState('');
+	const [image, setImage] = useState('');
+	const [abilities, setAbilities] = useState([]);
+	// const
+
+	//TODO: Task 1 - send http request to `https://pokeapi.co/api/v2/pokemon/1' and display the data
+	useEffect(() => {
+		async function fetchPokemon() {
+			const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+			const data = await response.json();
+			console.log(data);
+			setName(data.name);
+			setImage(data.sprites.front_default);
+			setAbilities(data.abilities.map(ability => ability.ability.name));
+		}
+		fetchPokemon();
+	});
+	// TODO: Task 2 - send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
+	// HINT: you will need useState and useEffect!
+	return (
+		<div className='pokemon-viewer'>
+			<h2>{name.length > 0 && `Name: ${name}`}</h2>
+			{id > 0 && <img src={image} alt='' />}
+			{abilities.length > 0 && (
+				<ul style={{ listStyle: 'none' }}>
+					Abilities:
+					{abilities.map((ability, index) => (
+						<li key={index}>{ability}</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 }
 
 export default PokemonViewer;
